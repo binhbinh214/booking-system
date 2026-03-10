8619ed6f488ef611dc2c4ad6eeadc8a6ae66030a
Trần Lương Bình <137519977+binhbinh214@users.noreply.github.com>
2026-03-10 13:22:22 +0700

    m

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -33,7 +33,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 30000, // 30 seconds
+  timeout: 60000, // 30 seconds
   withCredentials: true,
 });
 
